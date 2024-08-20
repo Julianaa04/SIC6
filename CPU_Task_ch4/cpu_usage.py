b168d6d94907221bff2dfcc86b9835f9f76f1d86
Ubuntu <ubuntu@ubuntu-2204.linuxvmimages.local>
2024-08-20 02:24:04 -0400

    Sensor_Visualize.py is added

--- CPU_Task_ch4/cpu_usage.py
+++ CPU_Task_ch4/cpu_usage.py
@@ -2,9 +2,9 @@
 import psutil
 from datetime import datetime
 
-#led_yellow = LED(20)
-#led_red = LED(21)
-#led_green = LED(27)
+led_yellow = LED(17)
+led_red = LED(22)
+led_green = LED(27)
 
 file = open("cpu_usage_loggg.txt", "a")
 
@@ -12,20 +12,20 @@ while True:
     cpu_usage = psutil.cpu_percent(interval = 5)
     print(f"Total CPU Usage: {cpu_usage}%")
 
-#    if cpu_usage < 50:
-#        led_green.on()
-#        led_yellow.off()
-#        led_red.off()
+    if cpu_usage < 50:
+        led_green.on()
+        led_yellow.off()
+        led_red.off()
 
-#    elif cpu_usage < 80:
-#        led_yellow.on()
-#        led_red.off()
-#        led_green.off()
+    elif cpu_usage < 80:
+        led_yellow.on()
+        led_red.off()
+        led_green.off()
 
- #   else:
- #       led_red.on()
- #       led_green.off()
- #       led_yellow.off()
+    else:
+        led_red.on()
+        led_green.off()
+        led_yellow.off()
 
     timestamp = f"{datetime.now().strftime('%Y/%m/%d %HH %MM %SS')}"
     Data = f"{timestamp} - CPU Usage: {cpu_usage}%\n"
